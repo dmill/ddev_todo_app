ffaad10c5f448a5dd2ba51cffd6f279145071309
Doug Mill <doug@empiri.co>
2017-07-06 15:36:23 -0500

    adding in some validations

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -2,4 +2,6 @@ class TodoItem < ApplicationRecord
   belongs_to :user
   belongs_to :asignee, class_name: "User"
   belongs_to :todo_list
+
+  validates_presence_of :summary
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :todo_lists
   has_many :todo_items
+
+  validates_presence_of :email, :first_name, :last_name, :dob
+  validates :email, length: { minimum: 6 }, uniqueness: true
+  validates_associated :todo_items
+  validates :email, exclusion: { in: ["me@example.com", "you@example.com"],
+    message: "%{value} is reserved." }
+
+  validates :user_type, inclusion: { in: [0, 1, 2],
+    message: "%{value} is an invalid user type" }
+
+
+  def todo_list_count
+    self.todo_lists.count
+  end
 end
