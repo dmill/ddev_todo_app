0580b966ee18368f12574a885521b22f90c42ea0
Jonathan Pugh <jdspugh@gmail.com>
2017-07-12 17:15:35 -0500

    Added logo, flash messages, css & user add form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,33 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    # p 'PARAMS'
+    # p params
+
+    @user = User.new(allowed_params)
+    if @user.save
+      flash[:notice] = 'User successfully created.'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def allowed_params
+    # return params.require(:user).permit(:first_name, :last_name, :email, :dob)
+    return params.require(:user).permit(:first_name, :last_name, :email, :dob, :user_type)
   end
 end
